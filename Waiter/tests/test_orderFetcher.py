da8ccb5d3d0f9c32f57f8466a7eb18adab02712c
Wing-kin-chan <Wing_kin_chan@hotmail.com>
2024-07-31 21:55:28 +0100

    +Waiter directory for fetching orders, order queue optimization, and displaying orders to baristas. +Tests for fetching orders

---
+++ Waiter/tests/test_orderFetcher.py
@@ -0,0 +1,34 @@
+import pytest
+from datetime import date, time
+from Waiter.app.scripts.orders import fetchOrder
+
+def test_fetchOrders():
+    order = fetchOrder.fetchOrder()
+    assert hasattr(order, 'customer')
+    assert hasattr(order, 'date')
+    assert hasattr(order, 'time')
+    assert hasattr(order, 'drinks')
+
+    assert isinstance(order.customer, str)
+    assert isinstance(order.date, date)
+    assert isinstance(order.time, time)
+    assert isinstance(order.drinks, list)
+
+    for drink in order.drinks:
+        assert hasattr(drink, 'customer')
+        assert hasattr(drink, 'drink')
+        assert hasattr(drink, 'milk')
+        assert hasattr(drink, 'shots')
+        assert hasattr(drink, 'temperature')
+        assert hasattr(drink, 'texture')
+        assert hasattr(drink, 'options')
+
+        assert isinstance(drink.customer, str)
+        assert isinstance(drink.drink, str)
+        assert isinstance(drink.milk, str)
+        assert isinstance(drink.shots, int)
+        assert isinstance(drink.temperature, str)
+        assert isinstance(drink.texture, str)
+        assert isinstance(drink.options, list)
+        assert all(isinstance(option, str) for option in drink.options)
+        
