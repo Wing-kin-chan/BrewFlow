04e2c08630e2b885b8e537f70567e7aba50f3c31
Wing-kin-chan <Wing_kin_chan@hotmail.com>
2024-09-10 17:00:00 +0100

    Moved queueManager logic to __init__ file, restructuring

---
+++ Manager/app/scripts/queueManager/fetchOrder.py
@@ -0,0 +1,31 @@
+import requests
+from Menu import Order
+import logging
+
+logging.basicConfig(level = logging.DEBUG)
+
+orderAPI_endpoint = 'http://127.0.0.1:8000/random_order'
+
+def fetchOrder() -> Order|None:
+    '''Returns an order that is randomly generated from the /order HTTP endpoint.'''
+    try:
+        response = requests.get(orderAPI_endpoint)
+    except requests.exceptions.ConnectTimeout as e:
+        logging.error(f'Connection timed out: {e}')
+        return None
+    except requests.exceptions.TooManyRedirects as e:
+        logging.error(f'Too many redirects: {e}')
+        return None
+    except requests.exceptions.RequestException as e:
+        logging.error(f'Request error: {e}')
+        return None
+    
+    try:
+        responseData: str = response.json()
+    except requests.JSONDecodeError as e:
+        logging.error(f'JSON Decode error: {e}')
+        return None
+
+    order = Order.model_validate_json(responseData)
+
+    return order
