0c91c6ebff2aa2d2c924ca1950c665e45a0af2fc
Wing-kin-chan <Wing_kin_chan@hotmail.com>
2024-09-10 17:04:02 +0100

    New routing, /random_order to generate random order, /send_order to send HTTP post to specified URL

--- Orders/main.py
+++ Orders/main.py
@@ -1,11 +1,46 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from Orders.app.generate_order import generateOrder
 from Menu import Order
+import httpx, json, logging
 
+logging.basicConfig(level = logging.DEBUG)
 app = FastAPI()
 
-@app.get('/order', response_model = Order)
+@app.get('/', response_class = HTMLResponse)
+def home():
+    pageHTML = """
+    <html>
+    <head>
+        <title>FastAPI Form</title>
+    </head>
+    <body>
+        <h1>Enter a URL</h1>
+        <form action="/send_order" method="post">
+            <label for="url">HTTP Address:</label>
+            <input type="text" id="url" name="url" placeholder="Enter URL here">
+            <button type="submit">Send</button>
+        </form>
+    </body>
+    </html>
+    """
+    return pageHTML
+
+@app.post("/send_order")
+async def sendOrder(url: str = Form(...)):
+    order: Order = generateOrder()
+    data: dict = json.loads(order.model_dump_json())
+    logging.info(f"Sending Order: {data}")
+
+    async with httpx.AsyncClient() as client:
+        await client.post(
+            url,
+            headers = {"Content-Type": "application/json"}, 
+            json = data
+        )
+        return RedirectResponse(url = "/", status_code = 303)
+
+@app.get('/random_order', response_model = Order)
 def getOrder():
     order = generateOrder()
     return JSONResponse(order.model_dump_json())
